3e2255b31a224eb92ef5b6e38bef2e3257ef8039
Keshav singh <61233273+keshavsingh123@users.noreply.github.com>
2020-07-30 16:15:12 +0530

    Initial commit

---
+++ src/components/Heading.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Heading() {
+  return <h1 className="heading"> My favourite food</h1>;
+}
+export default Heading;
---
+++ src/components/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function list() {
+  return (
+    <ul>
+      <li>Bacon</li>
+      <li>jackon</li>
+      <li>Noodles</li>
+    </ul>
+  );
+}
+export default list;
